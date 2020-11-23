a991820cdcbdbeef6c228a9662486d0a7824ad7c
Andrea Giacobino <no.andrea@gmail.com>
2020-11-24 00:30:51 +0100

    feat: add naive parser implementation

--- addresser/main.py
+++ addresser/main.py
@@ -20,21 +20,88 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from . import APP, _version
-from .parser import parse
 import argparse
 import logging
+from . import APP, _version
+from .parser import parse, Address
+from faker import Faker
+import time
+import json
 
 
-def cmd_parse_once(args):
+def _to_json(a: Address, verbose: bool = False) -> str:
+    if verbose:
+        return json.dumps({
+            "street": a.street,
+            "housenumber": a.number,
+            "src": a.src,
+        })
+    return json.dumps({
+        "street": a.street,
+        "housenumber": a.number
+    })
+
+
+def cmd_parse(args):
     """
     Parse one address
+
+    Args:
+        args: command line arguments
     """
     try:
         a = parse(args.data)
-        print(a)
+        print(_to_json(a, a.verbose))
     except Exception as err:
         print(f"Error massaging data: {err}")
+
+
+def cmd_parse_file(args):
+    """
+    Parse a file containing addresses to their structured format
+    """
+    print(f"will process entries from {args.input} to {args.output}")
+    start, count = time.time(), 0
+    with open(args.input) as fr, open(args.output, "w") as fw:
+        for line in fr:
+            record = parse(line)
+            fw.write(_to_json(record, args.verbose))
+            count += 1
+    print(f"completed: processed {count} lines in {time.time() - start}")
+
+
+def cmd_generate_address(args):
+    print(f"will generate {args.num} random addresses to {args.output}")
+    start = time.time()
+    locales = ['en', 'de', 'es', 'la',
+               'ar_AA', 'ar_EG', 'ar_JO',
+               'ar_PS', 'ar_SA', 'bg_BG',
+               'bs_BA', 'cs_CZ', 'dk_DK',
+               'el_CY', 'el_GR', 'et_EE',
+               'fa_IR', 'fi_FI', 'fil_PH',
+               'fr_CH', 'fr_FR', 'fr_QC',
+               'he_IL', 'hi_IN', 'hr_HR',
+               'hu_HU', 'hy_AM', 'id_ID',
+               'it_IT', 'ja_JP', 'ka_GE',
+               'ko_KR', 'lb_LU', 'lt_LT',
+               'lv_LV', 'mt_MT', 'ne_NP',
+               'nl_BE', 'nl_NL', 'no_NO',
+               'pl_PL', 'pt_BR', 'pt_PT',
+               'ro_RO', 'ru_RU', 'sk_SK',
+               'sl_SI', 'sv_SE', 'ta_IN',
+               'th_TH', 'tl_PH', 'tr_TR',
+               'tw_GH', 'uk_UA', 'zh_CN',
+               'zh_TW']
+
+    # create a new faker instance
+    Faker.seed(0)
+    f = Faker(locales)
+    # write the generated file
+    with open(args.output, "w") as fp:
+        for x in range(args.num):
+            fp.write(f.street_address())
+            fp.write("\n")
+    print(f"completed: generated {args.num} lines in {time.time() - start}")
 
 
 def cmd_version(args):
@@ -50,46 +117,92 @@ def main():
     # list commands
     commands = [
         {
-            'name': 'parse',
-            'help': 'Parse an input string into a structured address',
-            'target': cmd_parse_once,
-            'opts': [
+            "name": "parse",
+            "help": "Parse an input string into a structured address",
+            "target": cmd_parse,
+            "opts": [
+                {
+                    "names": ["data"],
+                    "help": "the string to prase",
+                },
+                {
+                    "names": ["-v", "--verbose"],
+                    "help": "add verbose json data (the original source)",
+                    "action": "store_true",
+                    "type": bool,
+                    "default": False
+                }
+            ]
+        },
+        {
+            "name": "parse-file",
+            "help": "Parse a address list file into a JSONlines file",
+            "target": cmd_parse_file,
+            "opts": [
+                {
+                    "names": ["-o", "--output"],
+                    "help": "the output JSONlines file name",
+                    "default": "addresses.jsonl"
+                },
                 {
                     "names": ["-i", "--input"],
-                    "help": "The input csv file (default data.csv)",
-                    "default": "data.csv"
+                    "help": "the input plain text file with addresses",
+                    "default": "addresses.txt"
+                },
+                {
+                    "names": ["-v", "--verbose"],
+                    "help": "add verbose json data (the original source)",
+                    "action": "store_true",
+                    "default": False
                 },
             ]
         },
         {
-            'name': 'parse-stdin',
-            'help': '''Read from the standard input and writes
-the result on the standard output''',
-            'target': cmd_parse_once,
-            'opts': []
+            "name": "generate-addresses",
+            "help": "Generate a file with a list of addresses",
+            "target": cmd_generate_address,
+            "opts": [
+                {
+                    "names": ["-o", "--output"],
+                    "help": "the file to write the list to",
+                    "default": "addresses.txt"
+                },
+                {
+                    "names": ["-n", "--num"],
+                    "help": "number of records to generate",
+                    "type": int,
+                    "default": 100,
+                },
+            ]
         },
         {
-            'name': 'version',
-            'help': 'Print the version and exit',
-            'target': cmd_version,
-            'opts': []
+            "name": "version",
+            "help": "Print the version and exit",
+            "target": cmd_version,
+            "opts": []
         },
 
     ]
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     subparsers.required = True
-    subparsers.dest = 'command'
+    subparsers.dest = "command"
     # register all the commands
     for c in commands:
-        subparser = subparsers.add_parser(c['name'], help=c['help'])
-        subparser.set_defaults(func=c['target'])
+        subparser = subparsers.add_parser(c["name"], help=c["help"])
+        subparser.set_defaults(func=c["target"])
         # add the sub arguments
-        for sa in c.get('opts', []):
-            subparser.add_argument(*sa['names'],
-                                   help=sa['help'],
-                                   action=sa.get('action'),
-                                   default=sa.get('default'))
+        for sa in c.get("opts", []):
+            kwargs = dict(
+                help=sa.get("help"),
+                default=sa.get("default"),
+                type=sa.get("type", str),
+            )
+            if sa.get("action") is not None:
+                del kwargs["type"]
+                kwargs["action"] = sa.get("action")
+            # parse the command
+            subparser.add_argument(*sa["names"], **kwargs)
 
     # parse the arguments
     args = parser.parse_args()
--- addresser/parser.py
+++ addresser/parser.py
@@ -1,5 +1,4 @@
-import time
-import json
+import re
 
 
 class Address(object):
@@ -7,33 +6,91 @@ class Address(object):
     Represent an Address with a street and a house number
     """
 
-    def __init__(self, street: str, number: str):
+    def __init__(self, street: str, number: str, src: str = None):
         """
         Constructor for Address
 
         Args:
             street(str): the street address
             number(str): the house number
+            src(str): the original address string
         """
         self.street = street
         self.number = number
+        self.src = src
 
     def __str__(self) -> str:
-        return json.dumps(self)
+        return f"street:{self.street};number:{self.number}"
+
+
+# Compile regexp before ues
+
+# check for house numbers at the beginning of an address string
+re_num_at_begin = re.compile(r"^\s*(no\.?)?\s*(\d+[a-z]*)(\/\d+[a-z]*)?\s*", re.IGNORECASE)
+# check for house numbers at the end of an address string
+re_num_at_end = re.compile(r"((no|д)\.?)?\s*(\d+\s*[a-z]*)(\/\d+[a-z]*)?\s*$", re.IGNORECASE)
+# matches a commat at the beginning or end of the input
+re_clean = re.compile(r"^,|,$")
+# matches references to apartments
+re_normalize = re.compile(r"(suite|apt\.?|appartamento|piso|к\.?)\s+\d+", re.IGNORECASE)
+
+
+def _clean(data: str) -> str:
+    """
+    Remove trailing spaces and commas
+
+    Args:
+        data(str): the data to clean
+    """
+    return re_clean.sub("", data.strip()).strip()
+
+
+def _normalize(src: str) -> str:
+    """
+    Normalize address string:
+
+    - Remove trailing spaces
+    - Remove parts that have to do with an apartment identifier
+
+    Args:
+        src(str): the string to normalize
+    Returns:
+        the normalized string
+    """
+    return re_normalize.sub("", src).strip()
 
 
 def parse(address: str) -> Address:
     """
-    Process the dataset csv input to a json optimized
-    for the rest API to serve
+    Parse an string representing a an address to a structured
+    Address object with the street name separated by the house number
 
     Args:
         address(str): the address to start
     Returns:
         an Address object parsed from the string input
     """
-    # track the execution time
-    start = time.time()
-
-    print(f"Completed in {time.time() - start}")
-    return Address("a", "b")
+    if address is None:
+        raise ValueError("None value not allowed")
+    # simple approach
+    # the house number is either at the beginning or in the end
+    src = _normalize(address)
+    # by default set street name as the address and empty house number
+    st, hn = (src, "")
+    # if it starts with a number then that's our street number:
+    m = re_num_at_begin.search(src)
+    if m is not None:
+        s, e = m.span()  # read match coordinates
+        hn = _clean(src[s:e])
+        st = _clean(src[e:])
+        return Address(st, hn, src)
+    # if it is in the end then there we go
+    m = re_num_at_end.search(src)
+    if m is not None:
+        s, e = m.span()  # read match coordinates
+        hn = _clean(src[s:e])
+        st = _clean(src[:s])
+        return Address(st, hn, src)
+    # if we got here it means that aither there is no house number or
+    # we are unable to identify it. As a fallback lets return the complete string
+    return Address(st, hn, src)
